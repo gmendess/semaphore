d9137a0f04745dd6e0b8f6137fc4b52659a5439a
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-06-05 02:10:06 -0300

    Estrutura inicial do projeto

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include "semaphore.h"
+
+int main() {
+
+  return 0;
+}
---
+++ semaphore.h
@@ -0,0 +1,6 @@
+#ifndef _SEMAPHORE_H
+#define _SEMAPHORE_H
+
+#include <pthread.h>
+
+#endif // _SEMAPHORE_H
